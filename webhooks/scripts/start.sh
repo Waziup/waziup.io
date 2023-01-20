77af7a76acc52a82e17d60e48806d6596b7173a3
Corentin Dupont <corentin.dupont@gmail.com>
2023-01-20 17:01:24 +0100

    Update start.sh

--- webhooks/scripts/start.sh
+++ webhooks/scripts/start.sh
@@ -13,4 +13,4 @@ echo ">>> Building master ..."
 bash /scripts/refresh.sh
 
 echo ">>> Starting webhook ..."
-/usr/bin/webhook -hooks /etc/hooks.json -verbose
+sudo /usr/bin/webhook -hooks /etc/hooks.json -verbose
