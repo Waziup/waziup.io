8717e53d4af4d3405c76814d46ded0178d079eef
Mohammad Nahin Islam <sec.tabrez03@gmail.com>
2023-02-21 15:01:11 +0100

    homa-banner bug fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,15 @@
 
 // Add active class to the current button (highlight it)
-var header = document.getElementsByClassName("nav-links");
-var links = header.getElementsByTagName("a");
-for (var i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
-}
+// var header = document.getElementsByClassName("nav-links");
+// var links = header.getElementsByTagName("a");
+// for (var i = 0; i < links.length; i++) {
+//   links[i].addEventListener("click", function() {
+//   var current = document.getElementsByClassName("active");
+//   current[0].className = current[0].className.replace(" active", "");
+//   this.className += " active";
+//   });
+// }
 
-
-
-/**** slider for the homePage banner ****/
 let slideIndex = 0;
 // showSlides();
 
