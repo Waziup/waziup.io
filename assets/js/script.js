cdf431f3fecc1c6b5ee6785e822b378499935077
Mohammad Nahin Islam <sec.tabrez03@gmail.com>
2023-04-24 15:50:40 +0200

    home banner: text separated from image

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const sliderContainers = document.querySelectorAll(".slider-container");
   sliderContainers.forEach(function (container, i) {
     const sliderBullets = container.querySelectorAll(".slider-nav > a");
-    const sliderImages = container.querySelectorAll(".slider > img");
+    const sliderImages = container.querySelectorAll(".slider > div");
 
     let index = 0;
     sliderBullets.forEach(function (bullet, i) {
