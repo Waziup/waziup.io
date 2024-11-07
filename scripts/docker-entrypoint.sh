6cb6740de59bf16602504c950d529a4252525658
Johann Forster <25847959+j-forster@users.noreply.github.com>
2024-11-07 12:47:37 +0100

    Fix docker-entrypoint sed in-place

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -ex
 
 # Modify hooks.json to use the correct branch and secret
-sed "s|refs/heads/master|refs/heads/$GIT_BRANCH|;s|secret-set-by-docker-entrypoint|$WEBHOOK_SECRET|" hooks.json
+sed -i "s|refs/heads/master|refs/heads/$GIT_BRANCH|;s|secret-set-by-docker-entrypoint|$WEBHOOK_SECRET|" hooks.json
 
 source start.sh
 
